65b1ff18c51e938e930ca21c2ea2b9aabbbc0555
Razvan Sdira <sdira_razvan@yahoo.com>
2016-11-14 15:09:35 +0200

    Add files via upload

---
+++ Exercise6.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Conditional_Statements
+{
+    class Exercise6
+    {
+        static void Main (String[] args)
+        {
+            int n;
+            Console.Write("Input any number: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            if (n < 0)
+                Console.WriteLine("The value of n = -1");
+            else if (n == 0)
+                Console.WriteLine("The value of n = 0.");
+            else
+                Console.WriteLine("The value of n = 1.");
+            Console.ReadLine();
+        }
+    }
+}
