5fad5fc99eb5a6d244e92c2059dae6660af23412
Razvan Sdira <sdira_razvan@yahoo.com>
2016-11-14 12:30:23 +0200

    Add files via upload

---
+++ Exercise3.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Conditional_Statements
+{
+    class Exercise3
+    {
+        static void Main (string[] args)
+        {
+            int x;
+            Console.Write("Input a number: ");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            if (x < 0)
+                Console.WriteLine("{0} is a negative nr.", x);
+            else
+                Console.WriteLine("{0} is a positive number.", x);
+            Console.ReadLine();
+        }
+    }
+}
