f41e1de26abe0cf6e4ee8996f7612b972eae4ab8
Razvan Sdira <sdira_razvan@yahoo.com>
2016-11-14 13:10:35 +0200

    Add files via upload

---
+++ Exercise5.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Conditional_Statements
+{
+    class Exercise5
+    {
+        static void Main (String[] args)
+        {
+            int x;
+            Console.Write("Input your age: ");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            if (x >= 18)
+                Console.WriteLine("Congratulation !!!! ");
+            else
+                Console.WriteLine("Sorry...");
+            Console.ReadLine();
+        }
+    }
+}
