0bb1068b7d2c3ef78de7beb1f650fe04dd4a35d8
Razvan Sdira <sdira_razvan@yahoo.com>
2016-11-14 16:19:59 +0200

    Add files via upload

---
+++ Exercise7.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Conditional_Statements
+{
+    class Exercise7
+    {
+        static void Main (String[] args)
+        {
+            int x;
+            Console.Write("Input your height: ");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            if (x < 150)
+                Console.WriteLine("The person is dwarf");
+            else if ((x >= 150) && (x <= 170))
+                Console.WriteLine("The person is normal");
+            else
+                Console.WriteLine("The person is tall.");
+            Console.ReadLine();
+        }
+    }
+}
