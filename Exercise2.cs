86f09d72a2b4f00111b6832256c6d049f45a339e
Razvan Sdira <sdira_razvan@yahoo.com>
2016-11-14 10:19:18 +0200

    Add files via upload

---
+++ Exercise2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Conditional_Statements
+{
+    class Exercise2
+    {
+        static void Main (String[] args)
+        {
+            int x, y;
+            Console.WriteLine("Input a number: ");
+            x = Convert.ToInt32(Console.ReadLine());
+            y = x % 2;
+
+            if (y == 0)
+                Console.WriteLine("{0} is even", x);
+            else
+                Console.WriteLine("{0} is odd.", x);
+
+            Console.ReadLine();
+        }
+    }
+}
