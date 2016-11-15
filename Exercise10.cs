8ae87307a1b6043d3db64cf1604cdaa6af7d7b2e
Razvan Sdira <sdira_razvan@yahoo.com>
2016-11-15 15:59:03 +0200

    Add files via upload

---
+++ Exercise10.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Conditional_Statements
+{
+    class Exercise10
+    {
+        static void Main (string[] args)
+        {
+            int x, y, z;
+            Console.WriteLine("Input the marks obtained in: ");
+            Console.Write("Pysics: ");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Chemistry: ");
+            y = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Mathematics: ");
+            z = Convert.ToInt32(Console.ReadLine());
+
+            if (x >= 65 && y >= 55 && z >= 50 && x + y + z >= 180 || x + (x + y + z) >= 140)
+                Console.WriteLine("The candidate is eligible for admission!");
+            else
+                Console.WriteLine("The candidate isn't eligible for admission.");
+            Console.ReadLine();
+        }
+    }
+}
