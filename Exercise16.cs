00147c9e98f9c470992e57a868c1bc954e66da13
Razvan Sdira <sdira_razvan@yahoo.com>
2016-11-16 14:10:53 +0200

    Add files via upload

---
+++ Exercise16.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Conditional_Statements
+{
+    class Exercise16
+    {
+        static void Main(string[] args)
+        {
+            char abc;
+            Console.Write("Input a leter: ");
+            abc = Convert.ToChar(Console.ReadLine());
+            if (abc == 'a' || abc == 'e' || abc == 'i' || abc == 'o' || abc == 'u')
+                Console.WriteLine("It's vowel.");
+            else
+                Console.WriteLine("It's a consonant.");
+            Console.ReadLine();
+        }
+    }
+}
